81cbb55d53f84d9697cf9e9739035f8b33ee13e5
ณัฐพร นาคพงษ์ <Missterdonat33@gmail.com>
2024-01-17 18:57:36 +0700

    feat:add authention registor and login

--- server/app.js
+++ server/app.js
@@ -3,15 +3,20 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import postRouter from "./apps/posts.js";
 import { client } from "./utils/db.js";
+import authRouter from "./apps/auth.js";
+import dotenv from "dotenv";
 
 async function init() {
+  dotenv.config();
+  
   const app = express();
   const port = 4000;
 
   await client.connect();
 
-  app.use(cors());
   app.use(bodyParser.json());
+  app.use(cors());
+  app.use("/auth", authRouter);
   app.use("/posts", postRouter);
 
   app.get("/", (req, res) => {
